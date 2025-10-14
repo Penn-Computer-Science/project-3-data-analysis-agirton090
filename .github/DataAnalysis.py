ec641a067bff0b356a376996c129c0e4e208625c
agirton090 <agirton090@phm.k12.in.us>
2025-10-14 09:51:03 -0400

    Update DataAnalysis.py

--- .github/DataAnalysis.py
+++ .github/DataAnalysis.py
@@ -2,36 +2,32 @@ import pandas as pd
 import random
 import matplotlib.pyplot as plt
 
-fNames = ["Danielle", "Noah", "Roman", "Ben" , "Van", "Alexis", "Katerina", "Andrew" ,"Kyan"]
-lNames = ["Esguerra", "Bradberry", "Williams", "Martin", "Latson", "Luebke", "Wedrychowicz", "Teddy", "Fogarty"]
-timetobed = ["8 - 10pm", "11 - 12am", "1am - 2am","3am - 4am"]
-howmuchsleep = ["1 - 4", "5 - 7", "8 - 10", "10+"]
+first_names = ["Danielle", "Noah", "Roman", "Ben" , "Van", "Alexis", "Katerina", "Andrew" ,"Kyan"]
+last_names = ["Esguerra", "Bradberry", "Williams", "Martin", "Latson", "Luebke", "Wedrychowicz", "Teddy", "Fogarty"]
+
+bed_times = ["8-10pm", "10-11pm", "11pm-12am","12am-1am", "1am-3am"]
+phone_use = ["Never", "Sometimes", "Often", "Always"]
+
 names = []
 
 for i in range(9):
-    names.append(f"{random.choice(fNames)} {random.choice(lNames)}")
+    names.append(f"{random.choice(first_names)} {random.choice(first_names)}")
 
 data = {
     "Name": names,
     "Age": [random.randint(14, 19) for _ in names ],
-    "GPA": [round(random.uniform(0.3, 4.0),2) for _ in names],
-    "Credits Completed": [random.randint(0, 60) for _ in names],
-    "Year": [random.choice(timetobed) for _ in names],
-    "Pathway": [random.choice(howmuchsleep) for _ in names]
+    "Bed Time": [random.choice([bed_times]) for _ in names],
+    "Sleep Hours": [random.randint(0, 60) for _ in names],
+    "Nap Before Bed": [random.randint(0, 5) for _ in names],
+    "Phone Before Bed": [random.choice([phone_use]) for _ in names],
+    "GPA": [round(random.uniform(0.3, 4.0),2) for _ in names]
 }
 
-pennData = pd.DataFrame(data)
 
-print(pennData)
+df = pd.DataFrame(data)
 
-df = pd.DataFrame(pennData)
-print(df.describe())
-print(df.info())
+
 print(df.groupby('Year')['GPA'].mean())
-df.groupby('Year')['GPA'].mean().plot(kind='bar')
-plt.title('Average Time to Bed')
-plt.xlabel('Times')
-plt.ylabel('GPA')
-
-
+df.groupby('Bed Time')['GPA'].mean().plot(kind='bar')
+plt.title('Average GPA by Bed Time')
 plt.show()
